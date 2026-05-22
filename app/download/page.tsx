cc0142747cad688ff15a9b54a6e3b8162a383dd1
kimseonnam <rimmentopro@gmail.com>
2026-05-22 15:43:40 +0900

    다운로드 링크 변경

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react'
 export default function DownloadPage() {
   useEffect(() => {
     window.location.href =
-      'https://drive.google.com/uc?export=download&id=1JwhC1tw0C4lMNmsG1RePu-vq1ku4PJuT'
+      'https://drive.google.com/file/d/1a3LHwvuQ_wPInU1_hTT4c3Ccc2BT1Sxu/view'
   }, [])
 
   return (
@@ -22,7 +22,7 @@ export default function DownloadPage() {
         fontWeight: 'bold',
       }}
     >
-      다운로드 중...
+      MIR CUP 다운로드 중...
     </div>
   )
 }
